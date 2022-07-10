22f2667179fffd696035a43b7fd0b5aefd491b2c
pcodelia <pedrocode@gmail.com>
2022-07-10 18:03:49 -0400

    primera version

---
+++ src/main/java/com/pokemon/legendario/LegendarioApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.legendario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendarioApplication.class, args);
+	}
+
+}
